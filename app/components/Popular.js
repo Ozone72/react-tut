376745989fe51841dc5d948b534d6cb13a02839a
Orin Fletcher <orinf@yahoo.com>
2018-02-19 20:21:52 -0800

    refactored Popular component to split out function from class

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,54 +1,52 @@
 var React = require("react");
 var PropTypes = require("prop-types");
 
-class SelectLanguage extends React.Component {
-  render() {
-    var languages = [
-      "All",
-      "JavaScript",
-      "Python",
-      "Ruby",
-      "Go",
-      "C#",
-      "Rust",
-      "CSS"
-    ];
-    return (
-      <ul className="languages">
-        {languages.map(function(lang) {
-          return (
-            //  have to use .bind to pass lang to initial function
-            <li
-              style={
-                lang === this.props.selectedLanguage
-                  ? { color: "#EC53FF" }
-                  : null
-              }
-              onClick={this.props.onSelect.bind(null, lang)}
-              key={lang}
-            >
-              {lang}
-            </li>
-          );
-        }, this)}
-      </ul>
-    );
-  }
+// stateless functional component
+function SelectLanguage(props) {
+  var languages = [
+    "All",
+    "JavaScript",
+    "Python",
+    "Ruby",
+    "Go",
+    "C#",
+    "Rust",
+    "CSS"
+  ];
+  return (
+    <ul className="languages">
+      {languages.map(function(lang) {
+        return (
+          //  have to use .bind to pass lang to initial function
+          <li
+            style={
+              lang === props.selectedLanguage ? { color: "#EC53FF" } : null
+            }
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}
+          >
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
+// require propTypes
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
 };
 
 class Popular extends React.Component {
-  //   adding a constructor to the class that sets initial state of the component
+  //   constructor that sets initial state of the component
   constructor(props) {
     super(props);
     this.state = {
       selectedLanguage: "All"
     };
-    // need to add this because in order for this.updateLanguage to be invoked correctly with the right context, we need to bind the this keyword (returns a new function)this.updateLanguage so that it's always invoked correctly
+    // in order for this.updateLanguage to be invoked correctly with the right context, we need to bind the 'this' keyword (returns a new function)this.updateLanguage so that it's always invoked correctly
     this.updateLangage = this.updateLangage.bind(this);
   }
   updateLangage(lang) {
