73aa11b28a5d952b44f434a829f18713b09b7279
Orin Fletcher <orinf@yahoo.com>
2018-02-22 21:24:52 -0800

    added Home component

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require("react");
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="home-container">
+        <h1>HOME</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Home;
