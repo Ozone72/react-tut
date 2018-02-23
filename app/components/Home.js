b0978144217f06e046833c7e93e2e078270d25df
Orin Fletcher <orinf@yahoo.com>
2018-02-23 08:57:56 -0800

    added button to Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,14 @@
 var React = require("react");
+var Link = require("react-router-dom").Link;
 
 class Home extends React.Component {
   render() {
     return (
       <div className="home-container">
-        <h1>HOME</h1>
+        <h1>There Can Be Only One!</h1>
+        <Link className="button" to="/battle">
+          Battle
+        </Link>
       </div>
     );
   }
